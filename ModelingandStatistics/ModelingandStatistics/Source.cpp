a477d2bd217c59a9a65dfe222253890cb06c846e
ryfles2 <rafalpolanin@gmail.com>
2018-01-10 18:56:46 +0100

    Zmienienie tablicy na tablicę dynamiczną, dodanie parametru do funkcji etap

--- ModelingandStatistics/ModelingandStatistics/Source.cpp
+++ ModelingandStatistics/ModelingandStatistics/Source.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 double boxMuler(double mi, double sigma);
-int etap(int liczbaPrzedmiotow, int nrSemestru, double poziomTrudnosci);
+int etap(int liczbaPrzedmiotow, int nrSemestru, double poziomTrudnosci, double * tabTrudnoœcZaliczeniaPrzedmiotu);
 long losowaniePrzedzial(long min, long max);
 double losowanie01();
 
@@ -16,13 +16,15 @@ int main()
 	int lPrzedmiotowSemestr[] = { 15,10,14,12,10,8,7 };
 	double poziomTrudnosci = 0.5;
 	int lDni = 0;
-	
-
+	double * tabTrudnoœcZaliczeniaPrzedmiotu = NULL;
+	int max = 0;
+	int min = 10000;
+	int zdane = 0;
 	for (int i = 0; i < 2000; i++)
 	{
 		for (int j = 0; j < 7; j++)
 		{
-			int pom = etap(lPrzedmiotowSemestr[j], j + 1, poziomTrudnosci);
+			int pom = etap(lPrzedmiotowSemestr[j], j + 1, poziomTrudnosci, tabTrudnoœcZaliczeniaPrzedmiotu);
 			if (pom == -1)
 			{
 				lDni = 0;
@@ -33,12 +35,15 @@ int main()
 				lDni += pom;	
 			}
 		}
-		if (lDni == 0)nieZdalo++;
-		else zdalo++;
+		if (max < lDni)max = lDni;
+		if (min > lDni && lDni != 0)min = lDni;
+		if (lDni != 0) zdane++;
 		cout << lDni << endl;
 		lDni = 0;
 	}
-	cout << "zdalo " << zdalo << " nie zdalo " << nieZdalo << endl;
+	//cout << "zdane " << zdane << endl;
+	//cout << "max " << max << endl;
+	//cout << "min " << min << endl;
 	return 0;
 }
 //losowanie z przedzia³u 
@@ -76,18 +81,27 @@ void losTab(double tab[], int size, double prog)
 	}
 
 }
-int etap(int liczbaPrzedmiotow, int nrSemestru, double poziomTrudnosci)
+int etap(int liczbaPrzedmiotow, int nrSemestru, double poziomTrudnosci, double * tabTrudnoœcZaliczeniaPrzedmiotu)
 {
 	int dniDodatkowe = 0;
 	const int dni = 152;
-	double * tabTrudnoœcZaliczeniaPrzedmiotu = new double[liczbaPrzedmiotow];
+	if (!tabTrudnoœcZaliczeniaPrzedmiotu)
+	{
+		tabTrudnoœcZaliczeniaPrzedmiotu = new double[liczbaPrzedmiotow];
+		for (auto i = 0; i < liczbaPrzedmiotow; i++)
+		{
+			tabTrudnoœcZaliczeniaPrzedmiotu[i] = 0;
+		}
+	}
+	
 	bool flag = true;
 	int pom = -1;
 
-
+	//inicjowanie tablicy
+	if (tabTrudnoœcZaliczeniaPrzedmiotu[0] == 0)
 	losTab(tabTrudnoœcZaliczeniaPrzedmiotu, liczbaPrzedmiotow, poziomTrudnosci);
 
-	//kana³ 
+	//kana³ na 4 semestrze losujemy wybran¹ specjalizacjê
 	if (nrSemestru == 4)
 	{
 		double wybor = losowanie01();
@@ -106,6 +120,7 @@ int etap(int liczbaPrzedmiotow, int nrSemestru, double poziomTrudnosci)
 		}
 	}
 
+	//obliczanie czy zdamy semestr w maxymalnie sesji poprawkowej
 	for (auto j = 0; j < 3; j++)
 	{
 		if (!flag)
@@ -134,41 +149,45 @@ int etap(int liczbaPrzedmiotow, int nrSemestru, double poziomTrudnosci)
 		if (tabTrudnoœcZaliczeniaPrzedmiotu[i] != 1) niezaliczone++;
 	}
 
+	//wyrzucenie ze studiów
 	if (niezaliczone > 4)
 	{
 		delete[] tabTrudnoœcZaliczeniaPrzedmiotu;
+		tabTrudnoœcZaliczeniaPrzedmiotu = NULL;
 		return -1;
 	}
+	//powtarzanie roku
 	else if (niezaliczone > 2)
 	{
+		flag = true;
 		if (nrSemestru == 1)
 		{
-			while (pom == -1)
+			while (pom != -1)
 			{
-				pom = etap(liczbaPrzedmiotow, 1, poziomTrudnosci);
+				pom = etap(liczbaPrzedmiotow, 1, poziomTrudnosci, tabTrudnoœcZaliczeniaPrzedmiotu);
+				cout << pom << endl;
 			}
 			dniDodatkowe += pom;
 		}
 		else
 		{
-			while (pom == -1)
+			while (pom != -1)
 			{
-				pom = etap(liczbaPrzedmiotow, nrSemestru - 1, poziomTrudnosci);
+				pom = etap(liczbaPrzedmiotow, nrSemestru - 1, poziomTrudnosci, tabTrudnoœcZaliczeniaPrzedmiotu);
+				cout << pom << endl;
 			}
 			dniDodatkowe += pom;
 		}
-	
-		delete[] tabTrudnoœcZaliczeniaPrzedmiotu;
-		return dni + dniDodatkowe;
 	}
+	//sesja poprawkowa
 	else if (niezaliczone > 0)
 	{
-		dniDodatkowe += losowaniePrzedzial(5, 10);
 		for (auto i = 0; i < liczbaPrzedmiotow; i++)
 		{
 			if (losowanie01() <= tabTrudnoœcZaliczeniaPrzedmiotu[i])
 			{
 				tabTrudnoœcZaliczeniaPrzedmiotu[i] = 1;
+				dniDodatkowe += losowaniePrzedzial(5, 10);
 			}
 		}
 	}
@@ -182,8 +201,10 @@ int etap(int liczbaPrzedmiotow, int nrSemestru, double poziomTrudnosci)
 	if (niezaliczone == 0)
 	{
 		delete[] tabTrudnoœcZaliczeniaPrzedmiotu;
+		tabTrudnoœcZaliczeniaPrzedmiotu = NULL;
 		return dni + dniDodatkowe;
 	}
 	delete[] tabTrudnoœcZaliczeniaPrzedmiotu;
+	tabTrudnoœcZaliczeniaPrzedmiotu = NULL;
 	return -1;
 }
